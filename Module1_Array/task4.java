7619187fff77c15dad61c6ecaa85d725e68d4446
AbhiKesava <kesavapatanamabhi@gmail.com>
2026-01-06 09:37:28 +0530

    Implement method to check sum parity in Demo class

--- Module1_Array/task4.java
+++ Module1_Array/task4.java
@@ -1,1 +1,21 @@
+class Demo {
+
+    public String add(int a, int b) {
+        if ((a + b) % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Demo obj = new Demo();   // object creation
+        String a = obj.add(10, 21);  // method call
+
+        System.out.println(a);  // output
+    }
+}
 
