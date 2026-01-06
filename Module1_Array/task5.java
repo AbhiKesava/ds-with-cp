7ce61fff13d753668d5c460876f8dc48a85537b1
AbhiKesava <kesavapatanamabhi@gmail.com>
2026-01-06 10:31:57 +0530

    Update task5.java

--- Module1_Array/task5.java
+++ Module1_Array/task5.java
@@ -1,1 +1,16 @@
+class Demo {
+    int age;
+}
 
+class Main {
+    public static void main(String[] args) {
+
+        System.out.println("jeet");
+
+        Demo a = new Demo();
+        System.out.println(a);
+
+        a.age = 21;
+        System.out.println(a.age);
+    }
+}
